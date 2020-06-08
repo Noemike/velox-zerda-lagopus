ff9307a37bee8f7ba9db40fb2ae50e1fdf7da44d
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-08 16:41:19 +0200

    background color changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
+console.log ('Noemi')
+
 $('#fox-1').text('Macrotis');
 
 $('#fox-2').css({border: '5px solid purple'});
 
-//$('p').css({background-color: 'yellow'})
+$('p').css("background-color","yellow");
